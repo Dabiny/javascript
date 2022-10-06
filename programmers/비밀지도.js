de6ce2370e11de1f4ef37448847b2f21b4e2c455
Dabiny <lina917@naver.com>
2022-10-06 15:59:41 +0900

    -

--- programmers/비밀지도.js
+++ programmers/비밀지도.js
@@ -37,3 +37,11 @@ function solution(n, arr1, arr2) {
 console.log(solution(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]));
 
 
+// 다른 해설 (숏코드) 
+function solution(n, arr1, arr2) {
+    return arr1.map((v, i) => addZero(n, (v | arr2[i]).toString(2)).replace(/1|0/g, a => +a ? '#' : ' '));
+}
+
+const addZero = (n, s) => {
+    return '0'.repeat(n - s.length) + s;
+}
---
+++ 딥다이브/45/45.2.js
@@ -0,0 +1,8 @@
+// const promise = new Promise((resolve, reject) => {
+//     if(/* 비동기 처리 성공시 */) {
+//         resolve('result');
+//     }
+//     else {
+//         reject('failure reason');
+//     }
+// });
