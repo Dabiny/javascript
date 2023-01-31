243c5e1bf4086387651f1d7b44b6d9d71889c4b1
Dabiny <lina917@naver.com>
2023-01-31 14:54:36 +0900

    -

--- programmers/Level3/네트워크.js
+++ programmers/Level3/네트워크.js
@@ -47,3 +47,49 @@ console.log(
     ])
 );
 // 다시 풀어보기 !!!!! for문을 돌면서 네트워크 개수를 찾는게 처음본 유형이였다. 
+
+// 복습
+function solution(n, computers) {
+    let answer = 0;
+    let check = Array.from({length: n}, () => 0); // n길이의 check배열 생성
+
+    function dfs(v) {
+        // 3. 깊이 탐색에 들어왔으면 자신(v)은 dfs에 방문한상태이기 때문에 check[v]에 방문했다는 체크를해준다. 
+        check[v] = 1; 
+
+        // 4. 현재 v에서 모든 컴퓨터를 탐색하며 자신과 연결되어있으면서, 한번도 방문하지않은 컴퓨터를 찾아야한다.  
+        for (let i = 0; i < n; i++) {
+            if(computers[v][i] === 1 && check[i] == 0) {
+                // 5. 그런 컴퓨터가 있다면 그 컴퓨터를 시작으로 다시 탐색에 들어간다. 
+                dfs(i);
+            }
+        }
+    }
+
+    // 1. 컴퓨터 개수가 주어지면 모든 컴퓨터를 확인하면서 다른컴퓨터와 연결되어있는지 확인해야한다. 
+    for (let i = 0; i < n; i++) {
+        // 2. i번의 컴퓨터를 방문해보지 않았다면 단독인 컴퓨터도 네트워크 하나로 치기때문에 answer++후 
+        // 다른컴퓨터와 연결되어있는지 깊이탐색을 시작해본다. 
+        if (!check[i]) {
+            answer++;
+            dfs(i);
+        }
+    }
+    return answer;
+}
+
+console.log(
+    solution(3, [
+        [1, 1, 0],
+        [1, 1, 0],
+        [0, 0, 1],
+    ])
+);
+
+console.log(
+    solution(3, [
+        [1, 1, 0],
+        [1, 1, 1],
+        [0, 1, 1],
+    ])
+);
--- programmers/Level3/야근지수.js
+++ programmers/Level3/야근지수.js
@@ -57,3 +57,45 @@ console.log(solution(1, [2, 1, 2]));
 console.log(solution(3, [1, 1]));
 
 // 최대힙 다시보기 (복습)
+function solution(works, n) {
+    let result = 0; // 최소 야근 피로도
+    let workSum = works.reduce((p, c) => p + c, 0); // 업무량 총합
+    if (workSum <= n) return 0; // 만약 남은시간보다 업무량이 적거나 같으면 시간안에 처리가능하므로 0리턴
+
+    // 1. 제곱은 근이 작을수록 제일 작은값이 나오기 때문에 큰 숫자를 먼저 상쇄시키는게 좋다. 
+    // 먼저 작업량을 오름차순으로 정렬시켜놓는다. 
+    works.sort((a, b) => a - b);
+
+    // 2. 남은시간n을 계속 상쇄시키면서 업무량을 깎아내줄 것이기 때문에 while문을 돌려준다. 
+    while(n !== 0) {
+        // 3. 제일 끝이 제일큰 숫자이기때문에 --해주고 n도 --해준다. 
+        works[works.length - 1]--;
+        n--;
+
+        // 4. 상쇄시켜주었다면 아직도 끝요소가 제일 큰지 확인을 해주어야한다. 
+        pq(works);
+        // 9. pq를 빠져나와서 그 다음 제일큰숫자가 뒤에있으니 그 숫자를 다시 --해주는 로직이다. 
+    }
+    function pq(works) {
+        // 5. 제일 큰 값을 pop 시켜서 변수에 저장후 비교할 것이다. 
+        const num = works.pop();
+        const length = works.length;
+
+        // 6. works의 그다음 큰 수와 비교할것이라서 거꾸로 탐색하는 for문을 정의
+        for (let i = length - 1; i >= 0; i--) {
+            // 7. 만약 가장큰 값 num이 그다음큰 요소 works[i]보다 크거나 같으면 다시 i뒤로 넣어주어야한다. 
+            //   
+            if (num >= works[i]) { 
+                works.splice(i + 1, 0, num);
+                break;
+            }
+        }
+
+        // 8. 만약 pop된게 works 요소들보다 작았다면 pop요소가 work요소들중 제일 작은 숫자인 뜻.
+        // 그렇기때문에 아직 pop된 요소는 다시 배열에 들어가지 않았다. 오름차순으로 정렬해놨으니 앞으로 넣어주자.
+        if (length === works.length) {
+            works.unshift(num);
+        }
+        
+    }
+}
